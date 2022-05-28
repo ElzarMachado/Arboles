9414def47bd526bcef3638c5a54a77b3aa5de581
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-28 12:59:11 -0700

    Cambios clase nodo

---
+++ ArbolesGenerales/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class Nodo
+    {
+        public string Dato { get; set; }  //prop tab tab
+        public Nodo Hijo { get; set; }
+        public Nodo Hermano { get; set; }
+
+        public Nodo(string dato, Nodo hijo = null, Nodo hermano = null) //ctor tab tab 
+        {
+            Dato = dato;
+            Hijo = hijo;
+            Hermano = hermano;
+        }
+    }
+}
